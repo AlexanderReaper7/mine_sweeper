e3f99fd6f99d480a3c0f0001ffe1eaef55b1c985
Alexander Öberg <stativetroller7@gmail.com>
2020-11-30 14:43:11 +0100

    starting UI

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,89 @@
-use std::{char::from_digit, io::*};
-use rand::prelude::*;
+mod mine_field;
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
 
+use graphics::{Graphics, clear, color::{BLACK, WHITE}, grid::Grid, line::Line, draw_state, math, rectangle};
+use mine_field::MineField;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
 
-struct Grid{
-    cols: usize,
-    rows: usize,
-    /// Contains all squares, a square is None if it contains a bomb and u8 by nimber of bombs adjacent
-    //squares: Vec<Vec<bool>> 
-    squares: Vec<Vec<Option<u8>>> 
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    mines: MineField,
+    grid: Grid,
+    line: Line,
 }
-impl Grid {
-    /// Returns an empty Grid (all None)
-    fn new(cols: usize, rows: usize) -> Self {
-        Self{cols, rows, squares: vec![vec![None;cols];rows]}
+
+pub fn grid<G>(grid: &Grid, line: &Line, transform: math::Matrix2d, g: &mut G)
+where G: Graphics
+{
+    grid.draw(line, &Default::default(), transform, g);
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(WHITE, gl);
+
+            let transform = c
+                .transform
+                .trans(x, y)
+                .trans(-25.0, -25.0);
+
+            // Draw a box rotating around the middle of the screen.
+            rectangle(BLACK, square, transform, gl);
+            grid(&self.grid, &self.line, transform, gl);
+        });
     }
 
-    pub fn generate_random_grid(cols: usize, rows: usize, mineChance: f64) -> Self {
-        let mut rng_thread = rand::thread_rng();
-        // generate mines
-        let mut squares: Vec<Vec<Option<u8>>>  = vec![vec![None;cols];rows];
-        for row in squares.iter_mut() {
-            for sq in row.iter_mut() {
-                *sq = if Grid::rand_func(&mut rng_thread, mineChance) {None} else {Some(0)}
-            }
-        }
-        // count mines in neighbouring squares
-        Grid::count_adjacent_mines(&mut squares);
-        return Self{cols, rows, squares};
-    }
-
-    fn count_adjacent_mines(squares: &mut Vec<Vec<Option<u8>>>) {
-        for row in 0..squares.len() {
-            for col in 0..squares[row].len() {
-                if squares[row][col].is_some() {
-                    for y in 0..3 {
-                        for x in 0..3 {
-                            if x == 1 && y == 1 {continue;} // skip self
-                            let y_sq = (row+y) as isize -1;
-                            let x_sq = (col+x) as isize -1;
-                            
-                            if y_sq < 0 || y_sq >= squares.len() as isize {continue;}
-                            if x_sq < 0 || x_sq >= squares[y_sq as usize].len() as isize {continue;}
-                            if squares[row+y-1][col+x-1].is_none() {
-                                match &mut squares[row][col] {
-                                    None => {},
-                                    Some(sq) => {
-                                        *sq += 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                }
-            }
-        }
-
-        // for (i, row) in squares.iter_mut().enumerate() {
-        //     for (j, col) in row.iter_mut().enumerate() {
-        //         match col {
-        //             Some(sq) =>       
-        //               for y in 0..3 {
-        //                 for x in 0..3 {
-        //                     if x == 1 && y == 1 {continue;} // skip self
-        //                     if let Some(a) = squares.get(i+y-1) {
-        //                         match a.get(j+x-1) {
-        //                             Some(_b) => match col {
-        //                                 Some(sq) => *sq += 1,
-        //                                 _ => {}
-        //                             },
-        //                             _ => {}
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //             _ => {}
-        //         }
-        //     }
-        // }
-
-    }
-
-    fn rand_func(rng: &mut ThreadRng, chance: f64) -> bool { //TODO: rename
-        let y = rng.gen_range(0.0, 1.0);
-        return y <= chance
-    }
-
-    fn print(&self) {
-        let mut output: String = "\n".to_string();
-        for row in self.squares.iter() {
-            for (i, sq) in row.iter().enumerate() {
-                output.push(if *sq == None {'X'} else {from_digit(sq.unwrap().into(), 10).unwrap()});  // add this square´s character to output
-                if (i + 1) % self.cols == 0 { // if at the end of row, add new-line character
-                    output.push('\n');
-                }  
-                else {  // else add space
-                    output.push(' ');
-                }
-            }
-        }
-        println!("{}", output);
-        stdout().flush();
+    fn update(&mut self, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        //self.rotation += 2.0 * args.dt;
     }
 }
 
 fn main() {
-    let grid = Grid::generate_random_grid(50, 12, 0.1);
-    grid.print()
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    let x_size = 800;
+    let y_size = 800;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new("Mine Sweeper", [x_size, y_size])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .samples(4)
+        .build()
+        .unwrap();
+
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        mines: MineField::generate_random_grid(12, 12, 0.2),
+        grid: Grid{cols: 12, rows: 12, units: 20.0},
+        line: Line::new(WHITE, 1.0)
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
+    }
 }
